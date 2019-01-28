85c1994b8bf45bac2c70c65a3407170ca5f65764
Kuba Mikołajczyk <qba600@gmail.com>
2019-01-28 12:52:03 +0100

    implement white background content popup / add magnific popup library for later video

--- resources/js/popups.js
+++ resources/js/popups.js
@@ -1,11 +1,37 @@
 $(document).ready(function() {
+    
+    /************************
+    ***  CONTENT POPUP ******
+    *************************/
 
       // Initialize the plugin
-      $('#big_room_popup').popup();
-      $('#small_room_popup').popup();
-      $('#living_room_popup').popup();
-      $('#bathroom_popup').popup();
+      $('#big_room_popup').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+      $('#small_room_popup').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+      $('#living_room_popup').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+      $('#bathroom_popup').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
 
       // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
       $.fn.popup.defaults.pagecontainer = '#page'
+       
+    
     });
