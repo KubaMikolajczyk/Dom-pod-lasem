2a1f27ae0b38d607a95ef1c5b65dc8631ee8c949
Kuba Mikołajczyk <qba600@gmail.com>
2019-01-28 21:56:55 +0100

    implement image popups

--- resources/js/popups.js
+++ resources/js/popups.js
@@ -29,6 +29,120 @@ $(document).ready(function() {
            transition: '0.3s',
            scrolllock: true
       });
+    
+      $('#big_room_img_1').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#big_room_img_2').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+      
+      $('#big_room_img_3').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+      
+      $('#big_room_img_4').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#small_room_img_1').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#small_room_img_2').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#small_room_img_3').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#small_room_img_4').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#living_room_img_1').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#living_room_img_2').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#living_room_img_3').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#living_room_img_4').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#bathroom_img_1').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#bathroom_img_2').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      $('#bathroom_img_3').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+     
+      $('#bathroom_img_4').popup({
+           color: 'white',
+           opacity: 1,
+           transition: '0.3s',
+           scrolllock: true
+      });
+    
+      
 
       // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
       $.fn.popup.defaults.pagecontainer = '#page'
