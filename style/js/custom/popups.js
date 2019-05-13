b0cefacc4ca15d3687842f6c16234b188f9731f3
Kuba Mikołajczyk <qba600@gmail.com>
2019-05-13 14:07:00 +0200

    popups.js / unable overflow of bg when modal on

--- style/js/custom/popups.js
+++ style/js/custom/popups.js
@@ -16,6 +16,9 @@ smallroomModal = document.getElementById('smallroomModal');
 livingroomModal = document.getElementById('livingroomModal');
 bathroomModal = document.getElementById('bathroomModal');
 
+// setting body variable for non scrolling effect
+var body = document.getElementsByTagName('body')[0];
+
 //Set up click listeners
 bigroomBtn.addEventListener('click', function openBigRoomModal() {
     openModal('bigroom');
@@ -38,15 +41,19 @@ function openModal(room) {
     switch(room){
         case 'bigroom':
             bigroomModal.style.display = "flex";
+            body.style.overflow = "hidden";
             break;
         case 'smallroom':
             smallroomModal.style.display = "flex";
+            body.style.overflow = "hidden";
             break;
         case 'livingroom':
             livingroomModal.style.display = "flex";
+            body.style.overflow = "hidden";
             break;
         case 'bathroom':
             bathroomModal.style.display = "flex";
+            body.style.overflow = "hidden";
             break;
         default:
             console.log("no such modal exists - look in style/js/popups.js")
