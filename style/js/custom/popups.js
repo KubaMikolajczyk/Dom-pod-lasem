9235622215b3a2b7f459980263829606fdac8c5c
Kuba Mikołajczyk <qba600@gmail.com>
2019-05-13 16:02:28 +0200

    Implement modal closing logic

--- style/js/custom/popups.js
+++ style/js/custom/popups.js
@@ -16,10 +16,19 @@ smallroomModal = document.getElementById('smallroomModal');
 livingroomModal = document.getElementById('livingroomModal');
 bathroomModal = document.getElementById('bathroomModal');
 
+// Setting close button variables
+var closeBigroomBtn, closeSmallroomBtn, closeLivingroomBtn, closeBathroomBtn;
+closeBigroomBtn = document.getElementById('closeBigroomBtn');
+closeSmallroomBtn = document.getElementById('closeSmallroomBtn');
+closeLivingroomBtn = document.getElementById('closeLivingroomBtn');
+closeBathroomBtn = document.getElementById('closeBathroomBtn');
+
 // setting body variable for non scrolling effect
 var body = document.getElementsByTagName('body')[0];
 
-//Set up click listeners
+
+
+//SET UP modal OPENING LISTENERS
 bigroomBtn.addEventListener('click', function openBigRoomModal() {
     openModal('bigroom');
 });
@@ -34,6 +43,23 @@ livingroomBtn.addEventListener('click', function openBigRoomModal() {
 
 bathroomBtn.addEventListener('click', function openBigRoomModal() {
     openModal('bathroom');
+});
+
+//SET UP close MODAL LISTENERS
+closeBigroomBtn.addEventListener('click', function closeBigroomModal() {
+    closeModal('bigroom');
+});
+
+closeSmallroomBtn.addEventListener('click', function closeSmallroomModal() {
+    closeModal('smallroom');
+});
+
+closeLivingroomBtn.addEventListener('click', function closeLivingroomModal() {
+    closeModal('livingroom');
+});
+
+closeBathroomBtn.addEventListener('click', function closeBathroomModal() {
+    closeModal('bathroom');
 });
 
 //open modal by simply changing it display features to flex from none
@@ -54,6 +80,29 @@ function openModal(room) {
         case 'bathroom':
             bathroomModal.style.display = "flex";
             body.style.overflow = "hidden";
+            break;
+        default:
+            console.log("no such modal exists - look in style/js/popups.js")
+    }
+}
+
+function closeModal(room){
+    switch(room){
+        case 'bigroom':
+            bigroomModal.style.display = "none";
+            body.style.overflow = "auto";
+            break;
+        case 'smallroom':
+            smallroomModal.style.display = "none";
+            body.style.overflow = "auto";
+            break;
+        case 'livingroom':
+            livingroomModal.style.display = "none";
+            body.style.overflow = "auto";
+            break;
+        case 'bathroom':
+            bathroomModal.style.display = "none";
+            body.style.overflow = "auto";
             break;
         default:
             console.log("no such modal exists - look in style/js/popups.js")
