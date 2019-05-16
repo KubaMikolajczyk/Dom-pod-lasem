750fd683c1b097b24b4b602ee67574b3c2f9db4d
Kuba Mikołajczyk <qba600@gmail.com>
2019-05-16 12:52:16 +0200

    REFACTOR popups.js
    Now instead of multiple listeners and multiple variables we have few arrays and listeners in for loops. We also have a system for checking if any modal is open to stop background scroll

--- style/js/custom/popups.js
+++ style/js/custom/popups.js
@@ -1,8 +1,9 @@
-// DISCLOSURE --> all modal features are setted up in style/css/myStyle.css
+// DISCLOSURE --> all modal styling is in style/css/myStyle.css
 
 
-// -----------------
+// -------------------------
 //VARIABLES
+//---------------------------
 
 
 // setting body variable for non scrolling effect
@@ -11,168 +12,87 @@ var body = document.getElementsByTagName('body')[0];
 // ----------------------------
 // ROOM MODAL CONTROL VARIABLES
 
-// Setting room picture buttons to variables
-var bigroomBtn, smallroomBtn, livingroomBtn, bathroomBtn;
-bigroomBtn = document.getElementById('bigroomBtn');
-smallroomBtn = document.getElementById('smallroomBtn');
-livingroomBtn = document.getElementById('livingroomBtn');
-bathroomBtn = document.getElementById("bathroomBtn");
+//setting array with all modals
+var modalsArray = document.getElementsByClassName('modal');
 
-//Setting room modal variables
-var bigroomModal, smallroomModal, livingroomModal, bathroomModal;
-bigroomModal = document.getElementById('bigroomModal');
-smallroomModal = document.getElementById('smallroomModal');
-livingroomModal = document.getElementById('livingroomModal');
-bathroomModal = document.getElementById('bathroomModal');
+// setting array with all opening buttons
+var openingBtnArray = document.getElementsByClassName('openBtn');
 
-// Setting close button variables
-var closeBigroomBtn, closeSmallroomBtn, closeLivingroomBtn, closeBathroomBtn;
-closeBigroomBtn = document.getElementById('closeBigroomBtn');
-closeSmallroomBtn = document.getElementById('closeSmallroomBtn');
-closeLivingroomBtn = document.getElementById('closeLivingroomBtn');
-closeBathroomBtn = document.getElementById('closeBathroomBtn');
+// setting array with all closing buttons
+var closingBtnArray = document.getElementsByClassName('closeBtn');
 
+// CHECKERS
 
-// ----------------------------
-// GALLERY MODAL CONTROL VARIABLES
-
-// setting gallery picture btn to variables
-var galleryBgRoomBtn1, galleryBgRoomBtn2, galleryBgRoomBtn3, galleryBgRoomBtn4;
-galleryBgRoomBtn1 = document.getElementById('galleryBgRoomBtn1');
-galleryBgRoomBtn2 = document.getElementById('galleryBgRoomBtn2');
-galleryBgRoomBtn3 = document.getElementById('galleryBgRoomBtn3');
-galleryBgRoomBtn4 = document.getElementById('galleryBgRoomBtn4');
-
-// Setting gallery modal variables
-var galleryBgRoom1, galleryBgRoom2, galleryBgRoom3, galleryBgRoom4;
-galleryBgRoom1 = document.getElementById('galleryBgRoom1');
-galleryBgRoom2 = document.getElementById('galleryBgRoom2');
-galleryBgRoom3 = document.getElementById('galleryBgRoom3');
-galleryBgRoom4 = document.getElementById('galleryBgRoom4');
-
+// this is used to flag if any popup is currently open
+var numModalsOpen = 0;
 
 
 //---------------------------
 // LISTENERS
-
-//SET UP room modal OPENING LISTENERS
-bigroomBtn.addEventListener('click', function openBigRoomModal() {
-    openModal('bigroom');
-});
-
-smallroomBtn.addEventListener('click', function openBigRoomModal() {
-    openModal('smallroom');
-});
-
-livingroomBtn.addEventListener('click', function openBigRoomModal() {
-    openModal('livingroom');
-});
-
-bathroomBtn.addEventListener('click', function openBigRoomModal() {
-    openModal('bathroom');
-});
-
-//SET UP close MODAL LISTENERS
-closeBigroomBtn.addEventListener('click', function closeBigroomModal() {
-    closeModal('bigroom');
-});
-
-closeSmallroomBtn.addEventListener('click', function closeSmallroomModal() {
-    closeModal('smallroom');
-});
-
-closeLivingroomBtn.addEventListener('click', function closeLivingroomModal() {
-    closeModal('livingroom');
-});
-
-closeBathroomBtn.addEventListener('click', function closeBathroomModal() {
-    closeModal('bathroom');
-});
+//---------------------------
 
 
-//-----------------------------
-//SET UP gallery modal OPENING LISTENERS
-galleryBgRoomBtn1.addEventListener('click', function galleryBgRoomBtn1() {
-    openModal('galleryBg1');
-})
 
-galleryBgRoomBtn2.addEventListener('click', function galleryBgRoomBtn2() {
-    openModal('galleryBg2');
-})
+// OPENING AND CLOSING LISTENERS
+for(var i = 0; i < openingBtnArray.length; i++){
+    (function() {
+        var index = i;
+        openingBtnArray[index].addEventListener('click', function(){
+            openModal(index);
+        })
+    }())
+}
 
-galleryBgRoomBtn3.addEventListener('click', function galleryBgRoomBtn3() {
-    openModal('galleryBg3');
-})
+for(var i = 0; i < closingBtnArray.length; i++){
+    (function() {
+        var index = i;
+        closingBtnArray[index].addEventListener('click', function(){
+            closeModal(index);
+        })
+    }())
+}
 
-galleryBgRoomBtn4.addEventListener('click', function galleryBgRoomBtn4() {
-    openModal('galleryBg4');
-})
+// WINDOW LISTENERS
+window.addEventListener('click', outsideClick);
 
 
-//open modal by simply changing it display features to flex from none
-function openModal(room) {
-    switch(room){
+//---------------------------
+//FUNCTIONS
+//---------------------------
+
+
+//open a specific modal from modal array
+function openModal(index){
+    modalsArray[index].style.display = 'flex';
+    body.style.overflow = "hidden";
+    numModalsOpen++;
+}
+
+//close a specific modal from modal array
+function closeModal(index){
+    modalsArray[index].style.display = 'none';
+    numModalsOpen--;
+    
+    //check if there is any open modal left
+    checkIfModalOpen();
+}
+
+//// close modal if outside click
+function outsideClick(e) {
+    for(var i = 0; i< modalsArray.length; i++){
+        if(e.target == modalsArray[i]){
+            modalsArray[i].style.display = 'none';
+            numModalsOpen--;
             
-        // ROOMS
-        case 'bigroom':
-            bigroomModal.style.display = "flex";
-            body.style.overflow = "hidden";
-            break;
-        case 'smallroom':
-            smallroomModal.style.display = "flex";
-            body.style.overflow = "hidden";
-            break;
-        case 'livingroom':
-            livingroomModal.style.display = "flex";
-            body.style.overflow = "hidden";
-            break;
-        case 'bathroom':
-            bathroomModal.style.display = "flex";
-            body.style.overflow = "hidden";
-            break;
-        
-        // GALLERY 
-        case 'galleryBg1':
-            galleryBgRoom1.style.display = "flex";
-            break;
-            
-        case 'galleryBg2':
-            galleryBgRoom2.style.display = "flex";
-            break;
-            
-        case 'galleryBg3':
-            galleryBgRoom3.style.display = "flex";
-            break;
-            
-        case 'galleryBg4':
-            galleryBgRoom4.style.display = "flex";
-            break;
-            
-        // DEFAULT
-        default:
-            console.log("no such modal exists - look in style/js/popups.js")
+            //check if there is any open modal left
+            checkIfModalOpen();
+       }
     }
 }
 
-function closeModal(room){
-    switch(room){
-        case 'bigroom':
-            bigroomModal.style.display = "none";
-            body.style.overflow = "auto";
-            break;
-        case 'smallroom':
-            smallroomModal.style.display = "none";
-            body.style.overflow = "auto";
-            break;
-        case 'livingroom':
-            livingroomModal.style.display = "none";
-            body.style.overflow = "auto";
-            break;
-        case 'bathroom':
-            bathroomModal.style.display = "none";
-            body.style.overflow = "auto";
-            break;
-        default:
-            console.log("no such modal exists - look in style/js/popups.js")
+// this function check if any modal is currently open and if there is none it enables scroll
+function checkIfModalOpen() {
+    if(numModalsOpen == 0){
+        body.style.overflow = "auto";
     }
 }
